9eed21bd92b803de6233b081ec63627efda2d2e0
ShorouqAdel <121319036+ShorouqAdel@users.noreply.github.com>
2024-06-21 00:44:51 +0300

    main.py

--- main.py
+++ main.py
@@ -72,6 +72,10 @@ async def identify_plant_with_images(organs: str = Form('auto'), image: UploadFi
         images=images
     )
 
+@app.head("/ping")
+async def ping():
+    return {"ping": "pong"}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8080)
